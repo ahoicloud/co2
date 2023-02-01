51c93b9a7f020524bee9caaf0e5534dd3cc3609d
Arne Breitsprecher <arnebr@gmail.com>
2023-02-01 14:23:22 +0000

    energeryPerByteComponent

--- src/Co2.php
+++ src/Co2.php
@@ -4,4 +4,28 @@ namespace Ahoicloud\Co2;
 
 class Co2
 {
+    const GIGABYTE = 1000* 1000 * 1000;
+    /**
+     * Accept a figure for bytes transferred and return an object representing
+     * the share of the total enrgy use of the entire system, broken down
+     * by each corresponding system component
+     * Gets emission by Sustainable Web Design
+     *   https://sustainablewebdesign.org/calculating-digital-emissions/.
+     *
+     * @param    $bytes
+     * @return array containing the energy in kilowatt hours, keyed by system component
+     */
+
+    public function  energyPerByteByComponent($bytes){
+        $transferedBytesToGb = $bytes / self::GIGABYTE;
+        $energyUsage = $transferedBytesToGb * config('co2.KWH_PER_GB');
+    
+        // return the total energy, with breakdown by component
+        return [
+          "consumerDeviceEnergy"=> $energyUsage * config('END_USER_DEVICE_ENERGY'),
+          "networkEnergy"=> $energyUsage * config('NETWORK_ENERGY'),
+          "productionEnergy"=> $energyUsage * config('PRODUCTION_ENERGY'),
+          "dataCenterEnergy"=> $energyUsage * config('DATACENTER_ENERGY'),
+        ];
+    }
 }
--- src/Commands/Co2Command.php
+++ src/Commands/Co2Command.php
@@ -2,18 +2,21 @@
 
 namespace Ahoicloud\Co2\Commands;
 
+use Ahoicloud\Co2\Co2;
 use Illuminate\Console\Command;
 
 class Co2Command extends Command
 {
     public $signature = 'co2';
 
-    public $description = 'My command';
+    public $description = 'Calculated Co2 emission';
 
     public function handle(): int
     {
-        $this->comment('All done');
-
+        $bytes=$this->ask('How many bytes?');
+        $co2=new Co2();
+        $result=$co2->energyPerByteByComponent($bytes);
+        $this->info('Result'. $result);
         return self::SUCCESS;
     }
 }
